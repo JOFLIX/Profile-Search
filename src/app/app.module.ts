1c3a40b9c75ad099f4b63252d064d7942fff6a7d
JOFLIX <joflixooko@outlook.com>
2019-07-13 14:35:58 +0300

    reconfigured importations to follow ng good practices of importation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { SearchUserService } from './searchUser.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { SearchUserService } from './searchUser.service';
+
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
